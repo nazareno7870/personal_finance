ffda2855ea0f7a727a60d166d0d8d09d4894fbd5
Nazareno <nazareno7870@gmail.com>
2022-02-02 19:33:51 -0300

    build: change route import

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,12 @@
 import Background from "../Backgorund/Background";
 import CardBalance from "../CardBalance/CardBalance";
 import LastTransactions from "../LastTransactions/LastTransactions";
-import TopBar from "../TopBar/TopBar.jsx";
 import NavBar from "../NavBar/NavBar";
 
 const HomePage = () => {
     return (
         <>
             <Background/>
-            <TopBar title={'Wallet'}/>
             <CardBalance/>
             <LastTransactions/>
             <NavBar/>
