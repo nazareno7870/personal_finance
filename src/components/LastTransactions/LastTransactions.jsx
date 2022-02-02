ccb8c1ee7599d88854c4c40a6f4d04d345a8b734
Nazareno <nazareno7870@gmail.com>
2022-02-02 15:54:49 -0300

    feat: Add Last Transactions Card

---
+++ src/components/LastTransactions/LastTransactions.jsx
@@ -0,0 +1,100 @@
+import './LastTransactions.css'
+const LastTransactions = () => {
+    return (
+    <div className="card-transactions">
+        <div className="title"><h3>Last Transactions</h3></div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>31 de Enero</p>
+            </div>
+            <div className="amount-transaction outflow">
+                <h3>-$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>28 de Enero</p>
+            </div>
+            <div className="amount-transaction entry">
+                <h3>+$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>31 de Enero</p>
+            </div>
+            <div className="amount-transaction outflow">
+                <h3>-$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>28 de Enero</p>
+            </div>
+            <div className="amount-transaction entry">
+                <h3>+$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>31 de Enero</p>
+            </div>
+            <div className="amount-transaction outflow">
+                <h3>-$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>28 de Enero</p>
+            </div>
+            <div className="amount-transaction entry">
+                <h3>+$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>31 de Enero</p>
+            </div>
+            <div className="amount-transaction outflow">
+                <h3>-$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>28 de Enero</p>
+            </div>
+            <div className="amount-transaction entry">
+                <h3>+$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>31 de Enero</p>
+            </div>
+            <div className="amount-transaction outflow">
+                <h3>-$9.686,86</h3>
+            </div>
+        </div>
+        <div className="item-transaction">
+            <div className="info-transaction">
+                <h3>Pago de Tarjeta</h3>
+                <p>28 de Enero</p>
+            </div>
+            <div className="amount-transaction entry">
+                <h3>+$9.686,86</h3>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default LastTransactions;
