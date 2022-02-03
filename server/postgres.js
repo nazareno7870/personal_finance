a2185db0a2acceb29c432400f0f6bc22a4823ce7
Nazareno <nazareno7870@gmail.com>
2022-02-03 14:43:07 -0300

    Deploy in heroku

--- server/postgres.js
+++ server/postgres.js
@@ -5,10 +5,7 @@ const connection = {
     host: 'ec2-34-230-110-100.compute-1.amazonaws.com',
     database: 'd6dh2o361qrtbq',
     password: process.env.PASSWORD,
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false,
-    }
+    port: 5432
 };
 
 module.exports = connection
