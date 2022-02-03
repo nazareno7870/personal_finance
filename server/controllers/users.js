e6a34e5c647e401b86f70082fe45bd743dfd8508
Nazareno <nazareno7870@gmail.com>
2022-02-03 20:02:40 -0300

    feat: Add login api with JsonWebToken

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const usersRouter = require('express').Router()
 const pg = require('pg');
 const connection = require('../postgres');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 usersRouter.post('/createuser', async (request, response) => {
 
@@ -33,6 +35,53 @@ usersRouter.post('/createuser', async (request, response) => {
                 response.status(401).send('That Mail is already associated with a user')
             }
             response.end()
+        } catch (error) {
+            response.status(401).send(error)
+        }
+
+    })
+
+
+    usersRouter.post('/login', async (request, response) => {
+
+        const { body } = request
+        const { email, password } = body
+      
+
+        const client = new pg.Client(connection);
+        try {
+
+            await client.connect();
+            const { rows } = await client.query('SELECT id_user, mail, password, name, date FROM public.users WHERE mail LIKE $1 ;', [email.toLowerCase()]);
+            await client.end();
+            if(!!rows.length){
+                const passwordCorrect = await bcrypt.compare(password, rows[0].password)
+
+                if (!passwordCorrect){
+                    return response.status(401).send('Invalid Email or Password')
+                  }
+
+                const userForToken = {
+                id: rows[0].id_user,
+                email: rows[0].mail
+                }
+            
+                const token = jwt.sign(
+                userForToken,
+                process.env.SECRET,
+                {
+                    expiresIn: 60 * 60 * 24 * 7
+                }
+                )
+                response.send({
+                    id: rows[0].id_user,
+                    email: rows[0].mail,
+                    token
+                })
+
+            }else{
+                response.status(401).send('Invalid Email or Password')
+            }
         } catch (error) {
             response.status(401).send(error)
         }
